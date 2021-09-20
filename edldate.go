1b12fe8b8c2094d75020c2b2c306986f796eab54
Victor Ma <pear_ma@msn.com>
2021-09-20 23:11:05 +0800

    add enum

--- edldate.go
+++ edldate.go
@@ -5,11 +5,47 @@ import (
 	"time"
 )
 
+type Freq int
+
+const (
+    Day Freq = iota
+    DayB
+    Week
+    Tenday
+    Month
+    Quarter
+    Halfyear
+    Year
+)
+
+func (f Freq) String() string {
+    switch f {
+    case Day:
+        return "Day"
+    case DayB:
+        return "DayB"
+    case Week:
+        return "Week"
+    case Tenday:
+        return "Tenday"
+    case Month:
+        return "Month"
+    case Quarter:
+        return "Quarter"
+    case Halfyear:
+        return "Halfyear"
+    case Year:
+        return "Year"
+    }
+    return "N/A"
+}
+
 type Edldate struct {
 	X time.Time
 	Y int
 }
 
 func (v *Edldate) Show() {
-	fmt.Println(v.Y + 10)
+    fmt.Printf("%s %d\n", Week,Year)
+	fmt.Println(v.Y + 15)
 }
